fc11714145d4b82fbcb0fde3566ccde3d3d564b8
Fernando Calisto <fernandocalisto.dev@gmail.com>
2026-05-27 12:12:57 -0300

    feat: Cria método e endpoint findAll para entidade category

--- src/main/java/com/fernandocalisto/MovieFlix/controller/CategoryController.java
+++ src/main/java/com/fernandocalisto/MovieFlix/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.fernandocalisto.MovieFlix.controller;
 
+import com.fernandocalisto.MovieFlix.entity.Category;
 import com.fernandocalisto.MovieFlix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController()
@@ -11,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @GetMapping()
+    public List<Category> getAllCategories() {
+        return categoryService.findAll();
+    }
 
 }
--- src/main/java/com/fernandocalisto/MovieFlix/service/CategoryService.java
+++ src/main/java/com/fernandocalisto/MovieFlix/service/CategoryService.java
@@ -1,7 +1,20 @@
 package com.fernandocalisto.MovieFlix.service;
 
+import com.fernandocalisto.MovieFlix.entity.Category;
+import com.fernandocalisto.MovieFlix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
 }
