d12acc211e5e7a64bf8cb2f1b67641e6be130a04
Fernando Calisto <fernandocalisto.dev@gmail.com>
2026-05-27 11:53:03 -0300

    feat: cria entidade Category

---
+++ src/main/java/com/fernandocalisto/MovieFlix/entity/Category.java
@@ -0,0 +1,19 @@
+package com.fernandocalisto.MovieFlix.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+}
