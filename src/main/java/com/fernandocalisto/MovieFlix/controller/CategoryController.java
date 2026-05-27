cd06555e3dd7d848a35b713790fd1a785b06bf65
Fernando Calisto <fernandocalisto.dev@gmail.com>
2026-05-27 12:03:12 -0300

    feat: criar service CategoryService

---
+++ src/main/java/com/fernandocalisto/MovieFlix/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package com.fernandocalisto.MovieFlix.controller;
+
+import com.fernandocalisto.MovieFlix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController()
+@RequestMapping("/movieflix/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+}
