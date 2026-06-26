768a960e548870d9013973eb520936d12011bafc
Fernando Calisto <fernandocalisto27@gmail.com>
2026-06-26 10:27:42 -0300

    feat: cria request e response para a entidade Movie

---
+++ src/main/java/com/fernandocalisto/MovieFlix/controller/request/MovieRequest.java
@@ -0,0 +1,12 @@
+package com.fernandocalisto.MovieFlix.controller.request;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record MovieRequest(String title,
+                           String description,
+                           LocalDate releaseDate,
+                           double rating,
+                           List<Long> categories,
+                           List<Long> streamings) {
+}
