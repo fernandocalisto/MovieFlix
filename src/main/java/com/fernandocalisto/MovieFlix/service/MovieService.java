b19149458bd8d3071d13762b887aa86377c59122
Fernando Calisto <fernandocalisto27@gmail.com>
2026-06-30 12:38:28 -0300

    refactor(movie): ajusta serialização de categorias e streamings no post

--- src/main/java/com/fernandocalisto/MovieFlix/service/MovieService.java
+++ src/main/java/com/fernandocalisto/MovieFlix/service/MovieService.java
@@ -2,11 +2,13 @@ package com.fernandocalisto.MovieFlix.service;
 
 import com.fernandocalisto.MovieFlix.entity.Category;
 import com.fernandocalisto.MovieFlix.entity.Movie;
+import com.fernandocalisto.MovieFlix.entity.Streaming;
 import com.fernandocalisto.MovieFlix.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,12 +17,16 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepository repository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public List<Movie> findAll() {
         return repository.findAll();
     }
 
     public Movie saveMovie(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return repository.save(movie);
     }
 
@@ -30,6 +36,22 @@ public class MovieService {
 
     public void deleteMovieById(Long id) {
         repository.deleteById(id);
+    }
+
+    public List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> {
+            categoryService.findCategoryById(category.getId()).ifPresent(categoriesFound::add);
+        });
+        return categoriesFound;
+    }
+
+    public List<Streaming> findStreamings (List<Streaming> streamings) {
+        List<Streaming> streamingList = new ArrayList<>();
+        streamings.forEach(streaming -> {
+            streamingService.findStreamingById(streaming.getId()).ifPresent(streamingList::add);
+        });
+        return streamingList;
     }
 
 }
